5ae1877b9aeebbcf63d0a0b894a59cf5766c82f7
Adelfjj <adelsonfranciscojj@outlook.com>
2021-08-25 23:59:04 -0300

    Trabalhando com formularios e templetes

--- src/app/courses/course.ts
+++ src/app/courses/course.ts
@@ -1,10 +1,10 @@
 export class Course {
     id: number = 0;
-    name: string = '';
+    name: string= '';
     imageUrl: string = '';
     price: number = 0;
     code: string = '';
-    duration: number = 0;
+    duration: number= 0;
     rating: number = 0;
     releaseDate: string = '';
     description: string = '';
